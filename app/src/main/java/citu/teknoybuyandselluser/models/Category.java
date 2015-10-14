75dba1cedf665aa8abe47db36a0bc6c79c97e9ff
Jacquelyn Amaya <amaya.jacque@gmail.com>
2015-10-14 15:05:09 +0800

    Categorize Buy Items
    But not yet tested

---
+++ app/src/main/java/citu/teknoybuyandselluser/models/Category.java
@@ -0,0 +1,38 @@
+package citu.teknoybuyandselluser.models;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Category {
+
+    private static final String TAG = "Category";
+
+    private String categoryName;
+    private int categoryId;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public static String[] getAllCategories(JSONArray jsonArray){
+        String[] categories = new String[jsonArray.length()];
+
+        for (int i = 0; i < categories.length; i++) {
+            try {
+                JSONObject category = jsonArray.getJSONObject(i);
+                categories[i] = category.getString("category_name");
+            } catch (JSONException e) {
+                Log.e(TAG, "Exception while getting category name", e);
+            }
+        }
+
+        return categories;
+    }
+}
